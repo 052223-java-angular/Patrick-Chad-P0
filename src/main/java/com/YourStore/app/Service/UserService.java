026a2ce946504f57902f2fa4e524e3f7dbf7427d
Patrick Powers <patrickpowers221@gmail.com>
2023-05-24 18:14:06 -0400

    FIrst commit after git wiped it

--- src/main/java/com/YourStore/app/Service/UserService.java
+++ src/main/java/com/YourStore/app/Service/UserService.java
@@ -1,5 +1,20 @@
 package com.YourStore.app.Service;
 
+import com.YourStore.app.DAO.UserDAO;
+import com.YourStore.app.Model.User;
+
 public class UserService {
-    
+    private final UserDAO userdao;
+    private final RoleService roleservice;
+
+    public UserService(UserDAO userdao, RoleService roleservice)
+    {
+        this.userdao = userdao;
+        this.roleservice = roleservice;
+    }
+
+    public User register(String username, String password)
+    {
+        
+    }
 }
