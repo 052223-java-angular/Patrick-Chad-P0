32af4ff40a0be0db7980c96808df97151119d1ea
cmrotruck <crotruck@gmail.com>
2023-05-27 06:17:18 -0400

    added CartItems and updated carts to only have relation with user.

---
+++ src/main/java/com/revature/ecommerce/models/Category.java
@@ -0,0 +1,17 @@
+package com.revature.ecommerce.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Category {
+    String id;
+    String name;
+}
